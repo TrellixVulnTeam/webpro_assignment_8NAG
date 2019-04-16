666d55ec1af45a79f3dece24baf115e686f72d13
jacktnp <thanaponpkw@gmail.com>
2019-04-16 23:26:32 +0700

    :triangular_flag_on_post: API1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'thanaponw',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
@@ -118,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'polls/static')
+]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.db.models import Count
+from django.http import HttpResponse
+from polls.models import Poll, Question
 
-# Create your views here.
+def index(request):
+	#poll_list = Poll.objects.all()
+	poll_list = Poll.objects.annotate(question_count=Count('question'))
+	print(poll_list.query)
+	# for poll in poll_list:
+	# 	question_list = Question.objects.filter(poll_id=poll.id).count()
+	# 	poll.question_count = question_count
+
+	context = {
+		'page_title': "My Polls", 
+		'poll_list': poll_list
+	}
+	
+	return render(request, 'polls/index.html', context=context)
+
+def detail(request, poll_id):
+
+	poll = Poll.objects.get(pk=poll_id)
+
+	return render(request, 'polls/detail.html', { 'poll': poll })
