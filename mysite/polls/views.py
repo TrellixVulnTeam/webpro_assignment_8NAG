08dc8fc75def035334be3d2dea61801a7c4ffcaa
jacktnp <thanaponpkw@gmail.com>
2019-04-17 00:22:06 +0700

    :triangular_flag_on_post: HTML_FORM

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count
 from django.http import HttpResponse
-from polls.models import Poll, Question
+from polls.models import Poll, Question, Answer
 
 def index(request):
 	#poll_list = Poll.objects.all()
@@ -17,9 +17,29 @@ def index(request):
 	}
 	
 	return render(request, 'polls/index.html', context=context)
-
+  
 def detail(request, poll_id):
 
 	poll = Poll.objects.get(pk=poll_id)
 
+	if request.method == 'POST':
+		for question in poll.question_set.all():
+			name = 'choice' + str(question.id)
+			choice_id = request.POST.get(name)
+			if choice_id:
+				try:
+					ans = Answer.objects.get(question_id=question.id)
+					ans.choice_id = choice_id
+					ans.save()
+				except Answer.DoesNotExist:
+					Answer.objects.create(choice_id=choice_id, question_id=question.id)
+			# print(choice_id)
+
+	print(request.GET)
+
 	return render(request, 'polls/detail.html', { 'poll': poll })
+ 
+# def create(request):
+# 	 if request.method == 'POST':
+# 		 title = request.POST.get('title')
+# 		 question_list = request.POST.getlist('questions[]')
